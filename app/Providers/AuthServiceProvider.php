04ede169268d1131dce77fc31fff91ff86665eb3
Jose Ramon Lopez <alumno@vm1>
2021-02-03 08:28:27 +0100

    Generando Policies

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -1,7 +1,8 @@
 <?php
 
 namespace App\Providers;
-
+use App\Models\Centro;  //Ojo que en los apuntes viene como post en lugar de centro
+use App\Policies\CentroPolicy; //Ojo que en los apuntes viene como post
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
 
@@ -14,6 +15,7 @@ class AuthServiceProvider extends ServiceProvider
      */
     protected $policies = [
         // 'App\Models\Model' => 'App\Policies\ModelPolicy',
+        Centro::class => CentroPolicy::class,
     ];
 
     /**
