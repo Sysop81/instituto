0d8e77f4e90ff57c636709c00d5871464642c08c
Jose Ramon Lopez <alumno@vm1>
2021-02-10 12:00:06 +0100

    Subiendo ficheros

--- routes/api.php
+++ routes/api.php
@@ -59,6 +59,9 @@ Route::group(['middleware' => 'auth:sanctum'], function() {
         return new UserResource($request->user());
     });
 
+    Route::post('/avatar', [\App\Http\Controllers\API\AvatarController::class, 'store']);
+    Route::get('/avatar', [\App\Http\Controllers\API\AvatarController::class, 'getAvatar']);
+
     Route::apiResource('centros', CentroController::class);
 
     Route::get('miCentro', function (Request $request) {
@@ -75,6 +78,7 @@ Route::group(['middleware' => 'auth:sanctum'], function() {
     Route::apiResource('niveles', NivelController::class)->parameters(['niveles' => 'nivel']); /* Debido a como trabaja laravel, el parámetro que usamos cuando por ejemplo queremos sacar un nivel
     en concreto (Ej: http://instituto.test/api/niveles/1), nos lo coge como "nivele" (laravel interpreta que el singular de niveles es nivele). Si no le indicamos a laravel que el singular de
     niveles es nivel, nos hará la consulta pero nos devolverá todo a null */
+
 });
 
 Route::get('centrosAPIRM', [CentroController::class, 'indexAPIRM']);
